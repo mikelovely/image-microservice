4631f52656e890dba279556db20325f2ac93674b
Mike Lovely <mike-lovely@hotmail.co.uk>
2016-12-03 22:42:44 +0000

    test site can work

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,18 @@
 use App\Storage\Exceptions\FileNotFoundException;
 
 $app->get('/{path}', function ($request, $response, $args) {
-    try {
-        $key = "image:{$args['path']}:{$_SERVER['QUERY_STRING']}";
+    var_dump("test");
+    exit;
+    // try {
+    //     $key = "image:{$args['path']}:{$_SERVER['QUERY_STRING']}";
 
-        $image = $this->cache->remember($key, null, function () use ($request, $args) {
-            return $this->image->load($this->storage->get($args['path'])->getContents())->withFilters($request->getParams())->stream();
-        });
+    //     $image = $this->cache->remember($key, null, function () use ($request, $args) {
+    //         return $this->image->load($this->storage->get($args['path'])->getContents())->withFilters($request->getParams())->stream();
+    //     });
 
-    } catch (FileNotFoundException $e) {
-        return $response->withStatus(404)->write('Not found');
-    }
+    // } catch (FileNotFoundException $e) {
+    //     return $response->withStatus(404)->write('Not found');
+    // }
 
-    return $response->withHeader('Content-Type', 'image/png')->write($image);
+    // return $response->withHeader('Content-Type', 'image/png')->write($image);
 });
